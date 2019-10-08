782efd9910018f6c27ae405ba61b5e273176691b
Volha <shaukavets@gmail.com>
2019-10-08 16:39:32 +0300

    add class implementation
    add class implementation

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.generatedNumber;
+        this.min;
+        this.max;
+    }
 
     setRange(min, max) {
-
+        this.min=min;
+        this.max=max;
     }
 
     guess() {
-
+       this.generatedNumber = Math.ceil((this.max - this.min)/2)+this.min;
+       return this.generatedNumber;
     }
 
     lower() {
-
+        this.max=this.generatedNumber;
     }
 
     greater() {
+        this.min=this.generatedNumber;
 
     }
 }
